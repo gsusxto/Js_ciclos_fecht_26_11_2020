33d43e1876f2212018cb5df178e19d86dfc2c347
unknown <=>
2020-11-26 19:41:58 -0500

    ciclos

---
+++ main.js
@@ -0,0 +1,67 @@
+document.write("conexion exitosa");
+
+//let permiso = false;
+
+// while(numero1<3){
+//     debugger;
+//     numero1++;
+//     console.log(numero1);
+// }
+
+// do{
+//     console.log(numero1);
+//     numero1++;
+    
+// }while(numero1<=3);
+
+// do{
+//     let user = prompt ("ingresar usurio");
+//     if(user=="Riki"){
+//         permiso=false;
+//         Document.write("inicio de sesion");
+        
+//     }
+// }while(permiso);
+
+let lista= ["gato","perro","gallina","elefante","delfin"];
+let lista_animales=[];
+lista_animales["felino"]="gato";
+lista_animales["canino"]="perro";
+lista_animales["ave"]="gallina";
+lista_animales["terrestre"]="elefante";
+lista_animales["acuatico"]="delfin";
+
+//console.log(lista_animales);
+
+for(datos in lista_animales){
+   console.log(lista_animales[datos]);
+ }
+
+
+
+// for (let i = 0; i < lista.length; i++) {
+//     console.log(lista[i]);
+// }
+// console.log("n");
+// for (let i = (lista.length-1); i >=0; i--) {
+//     console.log(lista[i]);
+// }
+
+// for(dato of lista){
+//     console.log(dato);
+// }
+
+// for(posicion in lista){
+//     console.log(posicion);
+// }
+
+
+// for (let i=0; i<6; i++){
+//     console.log(i);
+// }
+
+// for (let i=6; i>0; i-=2){
+//      console.log(i);
+// }
+
+
